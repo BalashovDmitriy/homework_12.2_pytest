38ab43d85efa49a977fb3a57e7a4c21ef9ad2669
Dmitriy.Balashov <reaver_std@mail.ru>
2023-06-08 22:08:53 +0500

    bugfixes

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,10 +1,10 @@
-import utils.dicts
+from utils.dicts import get_val
 
 
 def test_get_val():
     # {"vcs": "mercurial"}
-    assert utils.dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-    assert utils.dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
     # {}
-    assert utils.dicts.get_val({}, "vcs", "git") == "git"
-    assert utils.dicts.get_val({}, "vcs", "bazaar") == "bazaar"
+    assert get_val({}, "vcs", "git") == "git"
+    assert get_val({}, "vcs", "bazaar") == "bazaar"
